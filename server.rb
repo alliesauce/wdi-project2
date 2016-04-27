485497955a8190e5fa83adbb9cf837217f5f5b33
[A <[A[D>
2016-04-27 16:28:49 -0400

    add routes to server.rb

--- server.rb
+++ server.rb
@@ -1,7 +1,72 @@
 module Sinatra
   class Server < Sinatra::Base
-    get "/" do
+    get "/" do #get login instead?
       erb :index
     end
-  end
-end
+
+    get "/login" do
+      erb :index
+    end
+
+    post "/login" do
+      erb :index
+    end
+
+    post "/signup" do
+      erb :index
+    end
+
+    get "/dashboard" do
+      erb :index
+    end
+
+    get "/review/:id" do
+      erb :index
+    end
+
+    post "/review" do
+      erb :index
+    end
+
+    put "/review/:id" do
+      erb :index
+    end
+
+    post "/comment" do
+      erb :index
+    end
+
+    put "/comment/:id" do
+      erb :index
+    end
+
+    get "profile/:id" do
+      erb :index
+    end
+
+    put "profile/:id" do
+      erb :index
+    end
+
+    delete "/review/:id" do
+      erb :index
+    end
+
+    delete "/comment/:id" do
+      erb :index
+    end
+
+    def conn
+      if ENV["RACK_ENV"] == "production"
+        PG.connect(
+          dbname: ENV["POSTGRES_DB"], #ENV masks your variables for protection
+          host: ENV["POSTGRES_HOST"],
+          password: ENV["POSTGRES_PASS"],
+          user: ENV["POSTGRES_USER"]
+        )
+      else
+        PG.connect(dbname: "portfolio")
+      end
+    end #end def conn
+  end #end class Server
+end #end module Sinatra
